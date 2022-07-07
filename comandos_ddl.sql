b3b3ad41c4d20f9ba3af61e035ae2492a9d43ef0
Tiago Pereira <tiago.pereira@triersistemas.com.br>
2022-07-07 10:17:36 -0300

    views

--- comandos_ddl.sql
+++ comandos_ddl.sql
@@ -29,3 +29,22 @@ INSERT INTO turma_jovemdev(nom_turma) VALUES ('Matutino');
 
 select * from turma_jovemdev;
 select * from aluno_jovemdev;
+
+--------------- VIEWS
+CREATE OR REPLACE VIEW staff_vw AS
+    SELECT s.staff_id,
+           concat(s.first_name, ' ', s.last_name) as nome,
+           a.address,
+           a.postal_code,
+           a.phone,
+           c.city
+    FROM staff s
+    JOIN address a ON s.address_id = a.address_id
+    JOIN city c on a.city_id = c.city_id;
+
+
+SELECT s.nome, s.address, string_agg(rental_id::text, ';')
+FROM staff_vw s
+JOIN rental r on r.staff_id = s.staff_id
+group by s.nome, s.address
+;
